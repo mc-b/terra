f1e9c40b0f17a48301fd664c2c3448d7e8c34753
mc-b <marcel.bernet@mc-b.ch>
2026-04-07 17:06:50 +0200

    script Path angepasst

--- 03-aws/03-7-webshop/variables.tf
+++ 03-aws/03-7-webshop/variables.tf
@@ -11,19 +11,19 @@ variable "image" {
 # Scripts
 
 data "local_file" "order" {
-  filename  = "${path.module}/../scripts/order.yaml"
+  filename  = "${path.module}/../../scripts/order.yaml"
 }
 
 data "local_file" "customer" {
-  filename  = "${path.module}/../scripts/customer.yaml"
+  filename  = "${path.module}/../../scripts/customer.yaml"
 }
 
 data "local_file" "catalog" {
-  filename  = "${path.module}/../scripts/catalog.yaml"
+  filename  = "${path.module}/../../scripts/catalog.yaml"
 }
 
 data "local_file" "webshop" {
-  filename  = "${path.module}/../scripts/webshop.yaml"
+  filename  = "${path.module}/../../scripts/webshop.yaml"
 }
 
 
--- 03-azure/03-7-webshop/variables.tf
+++ 03-azure/03-7-webshop/variables.tf
@@ -10,19 +10,19 @@ variable "location" {
 # Scripts
 
 data "template_file" "order" {
-  template = file("${path.module}/../scripts/order.yaml")
+  template = file("${path.module}/../../scripts/order.yaml")
 }
 
 data "template_file" "customer" {
-  template = file("${path.module}/../scripts/customer.yaml")
+  template = file("${path.module}/../../scripts/customer.yaml")
 }
 
 data "template_file" "catalog" {
-  template = file("${path.module}/../scripts/catalog.yaml")
+  template = file("${path.module}/../../scripts/catalog.yaml")
 }
 
 data "template_file" "webshop" {
-  template = file("${path.module}/../scripts/webshop.yaml")
+  template = file("${path.module}/../../scripts/webshop.yaml")
 }
 
 
